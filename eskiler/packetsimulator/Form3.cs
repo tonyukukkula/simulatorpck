b765a001c1f5f4287b41e2bc15e730434617baf0
tonyukukkula <44534126+tonyukukkula@users.noreply.github.com>
2021-09-02 13:12:57 +0300

    calismayanlari buraya attik usta
    keşke çalışssalardı :(, 3 sürümde de aynı kod var 1i yarım 1i hiç 1i tam çalışıyor çok ilginç..

---
+++ eskiler/packetsimulator/Form3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace packetsimulator
+{
+    public partial class Form3 : Form
+    {
+        static Form1 form1 = new Form1();
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void gonder_Click(object sender, EventArgs e)
+        {
+            if (form1.radioButton2.Checked == true)
+                form1.m_port.Write(Encoding.ASCII.GetBytes(gondertext.Text), 0, Encoding.ASCII.GetBytes(gondertext.Text).Length);
+        }
+    }
+}
